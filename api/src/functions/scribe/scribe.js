dd28585dc834fd3420bb637f2c10915d83d19d26
Jace Benson <638764+jacebenson@users.noreply.github.com>
2022-12-05 23:51:08 -0600

    ...

--- api/src/functions/scribe/scribe.js
+++ api/src/functions/scribe/scribe.js
@@ -27,6 +27,7 @@ function respond({ code, data }) {
     headers: {
       'Content-Type': 'application/json',
       'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Methods': 'POST',
     },
     body: JSON.stringify(data),
   }
