6fa5288e3afafa39d2fd6c9f992e91934b611b5c
Jace Benson <638764+jacebenson@users.noreply.github.com>
2023-01-12 22:04:49 -0600

    adding jsonld

--- web/src/pages/HomePage/HomePage.js
+++ web/src/pages/HomePage/HomePage.js
@@ -1,6 +1,23 @@
 import { MetaTags } from '@redwoodjs/web'
 
 import AboutComponent from 'src/components/AboutComponent'
+let jsonLD = {
+  '@context': 'https://schema.org',
+  '@type': 'SoftwareApplication',
+  name: 'ScribeMonster',
+  operatingSystem: 'Google Chrome',
+  applicationCategory: 'Browser Extension',
+  aggregateRating: {
+    '@type': 'AggregateRating',
+    ratingValue: '5',
+    ratingCount: '1',
+  },
+  offers: {
+    '@type': 'Offer',
+    price: '0.00',
+    priceCurrency: 'USD',
+  },
+}
 const HomePage = () => {
   return (
     <>
@@ -14,7 +31,8 @@ const HomePage = () => {
         ogDescription="Scribemonster is a Chrome extension that helps with your code and access AI tools.  This is backend for a Chrome Extension, sign up with your email.  Let Stew the scribe monster take the initiative and get started on your client scripts, business rules, script includes, code reviews, and design work."
         ogImage="https://scribe.monster/header.png"
         ogImageAlt="ScribeMonster"
-      />
+      />{' '}
+      <script type="application/ld+json">{JSON.stringify(jsonLD)}</script>
       <AboutComponent />
     </>
   )
