7d9f5efee004f44b962cd11d4819d3ab7165158f
Jace Benson <638764+jacebenson@users.noreply.github.com>
2023-01-12 20:32:12 -0600

    added metatags to homepage

--- web/src/pages/HomePage/HomePage.js
+++ web/src/pages/HomePage/HomePage.js
@@ -1,5 +1,17 @@
+import { MetaTags } from '@redwoodjs/web'
+
 import AboutComponent from 'src/components/AboutComponent'
 const HomePage = () => {
-  return <AboutComponent />
+  return (
+    <>
+      <MetaTags
+        title="Scribemonster"
+        description="Scribemonster is a Chrome extension that helps with your code and access AI tools."
+        ogUrl="https://scribe.monster"
+        ogContentUrl="https://scribe.monster/icon-512.png"
+      />
+      <AboutComponent />
+    </>
+  )
 }
 export default HomePage
