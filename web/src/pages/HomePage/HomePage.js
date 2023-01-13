75b0ba9427ca5c9228a317091184aab720e9a58d
Jace Benson <638764+jacebenson@users.noreply.github.com>
2023-01-12 20:47:53 -0600

    updated seo

--- web/src/pages/HomePage/HomePage.js
+++ web/src/pages/HomePage/HomePage.js
@@ -9,6 +9,11 @@ const HomePage = () => {
         description="Scribemonster is a Chrome extension that helps with your code and access AI tools.  This is backend for a Chrome Extension, sign up with your email.  Let Stew the scribe monster take the initiative and get started on your client scripts, business rules, script includes, code reviews, and design work."
         ogUrl="https://scribe.monster"
         ogContentUrl="https://scribe.monster/header.png"
+        author="Jace Benson"
+        ogTitle="ScribeMonster"
+        ogDescription="Scribemonster is a Chrome extension that helps with your code and access AI tools.  This is backend for a Chrome Extension, sign up with your email.  Let Stew the scribe monster take the initiative and get started on your client scripts, business rules, script includes, code reviews, and design work."
+        ogImage="https://scribe.monster/header.png"
+        ogImageAlt="ScribeMonster"
       />
       <AboutComponent />
     </>
